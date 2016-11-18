6df46832ac516aacd3c686734cb04af8a8424346
Jonathan Freites <jfreites@users.noreply.github.com>
2016-11-18 14:01:37 -0600

    WIP Repository

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,16 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Album;
-use App\Repositories\AlbumRepository;
+#use App\Repositories\AlbumRepository;
 
 class HomeController extends Controller
 {
-	protected $repo;
+	/*protected $repo;
 
 	public function __construct(AlbumRepository $albumRepo)
 	{
 		$this->repo = $albumRepo;
-	}
+	}*/
 	/**
 	 * Show the home page
 	 * 
